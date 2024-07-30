54dc4b2e35735f7920b5809eb7dce3abeca26d3a
hikimain <datasai006@gmail.com>
2024-07-30 20:59:42 +0530

    update

--- application/views/workforce/edit.php
+++ application/views/workforce/edit.php
@@ -28,32 +28,32 @@
                         </div>
                         <?php echo form_open('workforce/update/'.$workforce['id'], array('id' => 'quickForm')); ?>
                         <div class="card-body row">
-                            <div class="form-group col-md-6">
+                            <div class="form-group col-md-4">
                                 <label for="name">Name</label>
                                 <input type="text" name="name" class="form-control" id="name"
                                     value="<?= $workforce['name'] ?>" required>
                             </div>
-                            <div class="form-group col-md-6">
+                            <div class="form-group col-md-4">
                                 <label for="address">Address</label>
                                 <textarea name="address" class="form-control" id="address"
                                     required><?= $workforce['address'] ?></textarea>
                             </div>
-                            <div class="form-group col-md-6">
+                            <div class="form-group col-md-4">
                                 <label for="description">Description</label>
                                 <textarea name="description" class="form-control" id="description"
                                     required><?= $workforce['description'] ?></textarea>
                             </div>
-                            <div class="form-group col-md-6">
+                            <div class="form-group col-md-4">
                                 <label for="aadhar_number">Aadhar Number</label>
                                 <input type="text" name="aadhar_number" class="form-control" id="aadhar_number"
                                     value="<?= $workforce['aadhar_number'] ?>" required>
                             </div>
-                            <div class="form-group col-md-6">
+                            <div class="form-group col-md-4">
                                 <label for="mobile_number">Mobile Number</label>
                                 <input type="text" name="mobile_number" class="form-control" id="mobile_number"
                                     value="<?= $workforce['mobile_number'] ?>" required>
                             </div>
-                            <div class="form-group col-md-6">
+                            <!-- <div class="form-group col-md-6">
                                 <label for="status">Status</label>
                                 <select name="status" class="form-control" id="status">
                                     <option value="active" <?= $workforce['status'] == 'active' ? 'selected' : '' ?>>
@@ -61,7 +61,7 @@
                                     <option value="inactive"
                                         <?= $workforce['status'] == 'inactive' ? 'selected' : '' ?>>Inactive</option>
                                 </select>
-                            </div>
+                            </div> -->
                         </div>
                         <div class="card-footer">
                             <button type="submit" class="btn btn-primary">Update Workforce</button>
